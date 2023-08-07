a938ede84bb8c6dcf49392bc806497469f3dd02a
bhwa233 <404174262@qq.com>
2023-08-07 13:04:31 +0800

    test

--- stories/Components/Button/Button.stories.ts
+++ stories/Components/Button/Button.stories.ts
@@ -1,3 +1,4 @@
+import { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -7,17 +8,22 @@ export default {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
+    docs: {
+      toc: true, // 👈 Enables the table of contents
+    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-};
+} as Meta<typeof Button>
+
+type Story = StoryObj<typeof Button>;
+
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary = {
+export const Primary: Story = {
   args: {
     primary: true,
     label: 'Button',
